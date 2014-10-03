15588ee05d28fbd71a89ecef335943bad10d314f
Silvia Clemente <sclemente10@gmail.com>
2014-10-03 14:11:40 +0200

    Bootstrap y estilos

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Pinteresting::Application.routes.draw do
-  get "pages/home"
+  resources :pins
+
+  get "say/hello"
+  get "say/goodbye"
+  root 'pages#home'
+  get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
